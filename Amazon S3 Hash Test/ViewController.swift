8a27fec3871466a644fdb112426d8b10a12668d5
Alfonso Tesauro <alfonso.tesauro@gmail.com>
2019-04-08 00:53:40 +0200

    Initial Commit

---
+++ Amazon S3 Hash Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amazon S3 Hash Test
+//
+//  Created by Alfonso Maria Tesauro on 08/04/2019.
+//  Copyright © 2019 Alfonso Maria Tesauro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
